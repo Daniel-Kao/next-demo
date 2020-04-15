e5cb7b6392e22c3529214fcf6815c349e9f3776b
Daniel-Kao <396007476@qq.com>
2020-04-15 21:34:28 +0800

    init
    Signed-off-by: Daniel-Kao <396007476@qq.com>

---
+++ pages/a.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+export default () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.red}>hah</div>
+    </div>
+  );
+};
